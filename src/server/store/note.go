4634561f1385c8217c8fb2b38b0b45dad850c589
Chris Shorrock <chris.shorrock@gmail.com>
2014-06-15 11:19:44 -0700

    more note serialization stuff with testing

--- src/server/store/note.go
+++ src/server/store/note.go
@@ -1,8 +1,27 @@
 package store
 
+import (
+	"encoding/json"
+	"github.com/shorrockin/noted/log"
+)
+
 type Note struct {
-	id      uint64 `json:"id"`
-	content string `json:"content"`
+	Id      uint64 `json:"id"`
+	Content string `json:"content"`
+}
+
+func (note Note) String() string {
+	bytes, error := json.Marshal(note)
+	if error != nil {
+		log.Warn("encountered error marshelling note: %v", error)
+		return ""
+	}
+	return string(bytes)
+}
+
+func (note *Note) FromJson(bytes []byte) (err error) {
+	err = json.Unmarshal(bytes, note)
+	return
 }
 
 // Note is a collection of all the notes we're currently managing.
@@ -19,7 +38,7 @@ func CreateNote(content string) (note Note) {
 // id to find it.
 func RemoveNote(id uint64) {
 	for index, note := range Notes {
-		if note.id == id {
+		if note.Id == id {
 			Notes = append(Notes[:index], Notes[index+1:]...)
 			return
 		}
--- src/server/store/note_test.go
+++ src/server/store/note_test.go
@@ -4,12 +4,44 @@ import (
 	"testing"
 )
 
+func TestJsonRendering(t *testing.T) {
+	note := &Note{1, "this is my note, isn't it great!?"}
+	stringd := note.String()
+
+	var unmarshelled Note
+
+	err := unmarshelled.FromJson([]byte(stringd))
+	if err != nil {
+		t.Errorf("could not unmarhel json structure: %v", err)
+	}
+
+	if unmarshelled.Id != note.Id {
+		t.Errorf("unmarshelled id did not match %v != %v", unmarshelled.Id, note.Id)
+	}
+
+	if unmarshelled.Content != note.Content {
+		t.Errorf("unmarshelled content did not match %v != %v", unmarshelled.Content, note.Content)
+	}
+
+	noIdContent := "{\"content\": \"hello my friend\"}"
+	noIdNote := &Note{}
+	err = noIdNote.FromJson([]byte(noIdContent))
+
+	if err != nil {
+		t.Errorf("could not unmarshel json structure with just content:", err)
+	}
+
+	if noIdNote.Content != "hello my friend" {
+		t.Errorf("expected 'hello my friend' not, got %v, from %v", noIdContent, noIdNote)
+	}
+}
+
 func TestCreateNote(t *testing.T) {
 	size := uint64(len(Notes))
 
 	note := CreateNote("this is a test note")
-	if note.id != size+1 {
-		t.Errorf("expected new note id to be %v but was %v", size+1, note.id)
+	if note.Id != size+1 {
+		t.Errorf("expected new note.Id to be %v but was %v", size+1, note.Id)
 	}
 
 	if uint64(len(Notes)) != size+1 {
@@ -24,17 +56,17 @@ func TestRemoveNote(t *testing.T) {
 	second := CreateNote("this is the second note")
 	third := CreateNote("this is the third note")
 
-	RemoveNote(second.id)
+	RemoveNote(second.Id)
 
 	if sizeNow := uint64(len(Notes)); sizeNow != size+2 {
 		t.Errorf("expected new note to be added to collection, wasn't size is now %v", sizeNow)
 	}
 
-	if last := Notes[len(Notes)-1]; last.id != third.id {
-		t.Errorf("expected the last note to be 'third' was %v", last.content)
+	if last := Notes[len(Notes)-1]; last.Id != third.Id {
+		t.Errorf("expected the last note to be 'third' was %v", last.Content)
 	}
 
-	if secondLast := Notes[len(Notes)-2]; secondLast.id != first.id {
-		t.Errorf("expected the second to last note to be 'first' was %v", secondLast.content)
+	if secondLast := Notes[len(Notes)-2]; secondLast.Id != first.Id {
+		t.Errorf("expected the second to last note to be 'first' was %v", secondLast.Content)
 	}
 }
